773364c987c25c1890bcf3de37751aab2dad8dc4
forhadisrafil <forhadtuhin5088@gmail.com>
2022-10-27 00:42:01 +0600

    new discord bot

--- Server Joiner Bot/Server Joiner Bot/Source Code/config.py
+++ Server Joiner Bot/Server Joiner Bot/Source Code/config.py
@@ -3,8 +3,3 @@ multiple = False
 delay_between_token_joins = 4
 timeout = 10
 
-
-'''
-i have 200 discord tokens to join the server.
-but i cant seem to make them join successfully. these are phone verified ones
-'''
